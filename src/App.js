402ca80a21e67233ee77d981c8861effa4e1d28e
ENG AYUUB <moztaphem9@gmal.com>
2023-07-13 09:42:50 +0300

    Reactjs tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(''));
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const handleSquareClick = (index) => {
+    if (board[index] === '' && !winner) {
+      const updatedBoard = [...board];
+      updatedBoard[index] = currentPlayer;
+      setBoard(updatedBoard);
+
+      checkWinner(updatedBoard, currentPlayer);
+      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    }
+  };
+
+  const checkWinner = (board, player) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (board[a] === player && board[b] === player && board[c] === player) {
+        setWinner(player);
+        break;
+      }
+    }
+  };
+
+  const renderSquare = (index) => (
+    <div className={`square ${board[index]}`} onClick={() => handleSquareClick(index)}>
+      {board[index]}
     </div>
   );
-}
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(''));
+    setCurrentPlayer('X');
+    setWinner(null);
+  };
+
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <div className="board">
+        {board.map((square, index) => renderSquare(index))}
+      </div>
+      {winner && (
+        <div className="winner">
+          <p>Player {winner} wins!</p>
+          <button onClick={resetGame}>Play Again</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
